d3ee5997b3da783a26f2842cedd56eabd167fbdd
Ramonzb24 <rzapater@ltu.edu>
2026-05-02 19:04:47 -0400

    Update full_system.launch.py

--- launch/full_system.launch.py
+++ launch/full_system.launch.py
@@ -109,8 +109,8 @@ def generate_launch_description():
         slam_launch,      # <--- Substituímos o slam_node por slam_launch
         lane_detection,
         lane_map,
-        navigation,
+        #navigation,
         obstacle_avoidance,
         behavior,
         rviz,
-    ])
+    )
